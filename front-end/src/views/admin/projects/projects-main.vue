042b96d214fb48ddfc6b371a50d001d54bd8b1a5
asepsunarya <asepsunarya839@gmail.com>
2024-01-16 06:51:50 +0700

    fix: ux

--- front-end/src/views/admin/projects/projects-main.vue
+++ front-end/src/views/admin/projects/projects-main.vue
@@ -44,7 +44,7 @@
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
             >
-              <div class="flex gap-x-2 items-center cursor-pointer">
+              <div class="flex gap-x-2 items-center">
                 <div
                   class="bg-gray-200 text-gray-500 rounded-full w-7 h-7 flex items-center justify-center"
                 >
@@ -63,7 +63,7 @@
                     }}
                   </span>
                 </div>
-                <div class="text-primary hover:underline">
+                <div>
                   {{ project.leader.name }}
                 </div>
               </div>
