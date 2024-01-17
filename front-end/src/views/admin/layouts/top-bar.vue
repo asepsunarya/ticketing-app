032890ba07e734115a2573dbc524b3e17ed01d36
asepsunarya <asepsunarya839@gmail.com>
2024-01-18 01:41:33 +0700

    fix: remove unused right bar

--- front-end/src/views/admin/layouts/top-bar.vue
+++ front-end/src/views/admin/layouts/top-bar.vue
@@ -147,30 +147,6 @@
                 <li>
                   <a
                     href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                    >Dashboard</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                    >Settings</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                    >Earnings</a
-                  >
-                </li>
-                <li>
-                  <a
-                    href="#"
                     @click="signOut()"
                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     role="menuitem"
